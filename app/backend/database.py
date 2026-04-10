c7fca3e187d74a4ea362108956ebe0dcbbe96b81
Borch Andrii <skabinausa@gmail.com>
2026-04-10 09:18:06 +0300

    feat: add database setup

---
+++ app/backend/database.py
@@ -0,0 +1,23 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from .models import Base
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./taskflowwithmedium.db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False}  # SQLite specific
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+def create_tables():
+    Base.metadata.create_all(bind=engine)
+
+def get_bd():
+    db = SessionLocal()
+    try:
+        yield db 
+    finally: # It will close even if there is an error
+        db.close()
